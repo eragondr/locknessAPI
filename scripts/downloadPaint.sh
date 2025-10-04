bc69c1f4b3afd564089776bd6c17bf4413ded03d
eragondr <thagsau@gmail.com>
2025-10-04 22:42:26 +0700

    remove code

--- scripts/downloadPaint.sh
+++ scripts/downloadPaint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-huggingface-cli login --token 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
+
 huggingface-cli download tencent/Hunyuan3D-2.1 --local-dir pretrained/tencent/Hunyuan3D21/hunyuan3d-paintpbr-v2-1 --include hunyuan3d-paintpbr-v2-1/*
 huggingface-cli download tencent/Hunyuan3D-2.1 --local-dir pretrained/tencent/Hunyuan3D21/hy3dpaint --include hy3dpaint/*
 huggingface-cli download tencent/Hunyuan3D-2.1 --local-dir pretrained/tencent/Hunyuan3D21/hy3dpaint --include hy3dpaint/*
