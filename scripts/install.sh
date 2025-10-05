f2eaa9b8fd309ddaac816ad0c30c43f0b150afef
eragondr <thagsau@gmail.com>
2025-10-05 18:51:55 +0700

    updadte install.sh

--- scripts/install.sh
+++ scripts/install.sh
@@ -40,8 +40,14 @@ fi
 
 #conda activate locknessapi
 
-echo "[INFO] Installing PyTorch with CUDA 12.8 support..."
+echo "[INFO] Download models"
+if [ "$ISMESH" == "1" ]; then
+    bash donwnloadMesh.sh
+else
+    bash donwnloadPaint.sh
+fi
 
+echo "[INFO] install Cuda"
 if [ "$CUDA_VER" == "121" ]; then
     pip install torch==2.4.0 torchvision==0.19.0 torchaudio==2.4.0 --index-url https://download.pytorch.org/whl/cu121
 elif [ "$CUDA_VER" == "124" ]; then
@@ -61,130 +67,7 @@ else
 fi
 
 
-## install pytorch for specific cuda versions
-#pip  install torch torchvision
-#if [ $? -eq 0 ]; then
-#    echo "[SUCCESS] PyTorch installation completed"
-#else
-#    echo "[ERROR] Failed to install PyTorch"
-#    exit 1
-#fi
 
-
-
-
-#echo ""
-#echo "========================================"
-#echo "Installing TRELLIS Dependencies"
-#echo "========================================"
-#### we startup with the environment of trellis ###
-#echo "[INFO] Changing directory to thirdparty/TRELLIS..."
-#cd thirdparty/TRELLIS
-#echo "[INFO] Running TRELLIS setup script..."
-#. ./setup.sh --basic --xformers --flash-attn --diffoctreerast --spconv --mipgaussian --kaolin --nvdiffrast
-#pip install kaolin -f https://nvidia-kaolin.s3.us-east-2.amazonaws.com/torch-2.4.0_cu121.html
-#if [ $? -eq 0 ]; then
-#    echo "[SUCCESS] TRELLIS setup completed"
-#else
-#    echo "[ERROR] TRELLIS setup failed"
-#    exit 1
-#fi
-## for systems with glibc < 2.29 , you may need to build kaolin from source manually
-#echo "[NOTE] For systems with glibc < 2.29, you may need to build kaolin from source manually"
-#
-#echo ""
-#echo "========================================"
-#echo "Installing PartField Dependencies"
-#echo "========================================"
-## install PartField for mesh segmentation
-#echo "[INFO] Changing directory to thirdparty/PartField..."
-#cd ../../thirdparty/PartField
-#echo "[INFO] Installing PartField core dependencies..."
-#pip install lightning==2.2 h5py yacs trimesh scikit-image loguru boto3
-#if [ $? -eq 0 ]; then
-#    echo "[SUCCESS] PartField core dependencies installed"
-#else
-#    echo "[ERROR] Failed to install PartField core dependencies"
-#    exit 1
-#fi
-#
-#echo "[INFO] Installing additional PartField dependencies..."
-#pip install mesh2sdf tetgen pymeshlab plyfile einops libigl polyscope potpourri3d simple_parsing arrgh open3d psutil
-#if [ $? -eq 0 ]; then
-#    echo "[SUCCESS] Additional PartField dependencies installed"
-#else
-#    echo "[ERROR] Failed to install additional PartField dependencies"
-#    exit 1
-#fi
-#
-#echo "[INFO] Installing PyTorch Geometric extensions..."
-#pip install torch-scatter torch_cluster -f https://data.pyg.org/whl/torch-2.4.0+cu121.html
-#if [ $? -eq 0 ]; then
-#    echo "[SUCCESS] PyTorch Geometric extensions installed"
-#else
-#    echo "[ERROR] Failed to install PyTorch Geometric extensions"
-#    exit 1
-#fi
-# installation for PartField end
-#echo "[SUCCESS] PartField installation completed"
-
-#echo ""
-#echo "========================================"
-#echo "Installing Hunyuan3D 2.0 Dependencies"
-#echo "========================================"
-#### install hunyuan3d for mesh generation  ###
-#echo "[INFO] Changing directory to thirdparty/Hunyuan3D-2..."
-#cd ../../thirdparty/Hunyuan3D-2
-#echo "[INFO] Installing Hunyuan3D 2.0 requirements..."
-#pip install -r requirements.txt
-#if [ $? -eq 0 ]; then
-#    echo "[SUCCESS] Hunyuan3D 2.0 requirements installed"
-#else
-#    echo "[ERROR] Failed to install Hunyuan3D 2.0 requirements"
-#    exit 1
-#fi
-#
-#echo "[INFO] Installing Hunyuan3D 2.0 package..."
-#pip install -e .
-#if [ $? -eq 0 ]; then
-#    echo "[SUCCESS] Hunyuan3D 2.0 package installed"
-#else
-#    echo "[ERROR] Failed to install Hunyuan3D 2.0 package"
-#    exit 1
-#fi
-#
-#echo "[INFO] Building custom rasterizer..."
-#cd hy3dgen/texgen/custom_rasterizer
-#python3 setup.py install
-#if [ $? -eq 0 ]; then
-#    echo "[SUCCESS] Custom rasterizer built successfully"
-#else
-#    echo "[ERROR] Failed to build custom rasterizer"
-#    exit 1
-#fi
-#
-#echo "[INFO] Building differentiable renderer..."
-#cd ../../..
-#cd hy3dgen/texgen/differentiable_renderer
-#python3 setup.py install
-#cd ../../../
-#if [ $? -eq 0 ]; then
-#    echo "[SUCCESS] Differentiable renderer built successfully"
-#else
-#    echo "[ERROR] Failed to build differentiable renderer"
-#    exit 1
-#fi
-#### installation for hunyuan3d 2.0 end ###
-#echo "[SUCCESS] Hunyuan3D 2.0 installation completed"
-
-#cd ../..
-
-echo "Current directory after cd: $(pwd)"
-if [ "$ISMESH" == "1" ]; then
-    bash donwnloadMesh.sh
-else
-    bash donwnloadPaint.sh
-fi
 cd ..
 
 
